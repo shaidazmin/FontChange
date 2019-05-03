7e151ee8e15f4607cc3dfdda3a43b91ac0cd9b62
Shaid Azmin <shaidazmin@gmail.com>
2019-05-03 20:13:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/nz/fontchange/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.nz.fontchange;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Typeface typeface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.two);
+        typeface.createFromAsset(getAssets(),"my_font.otf");
+        textView.setTypeface(typeface);
+
+
+    }
+}
